94a88ec38577918badd7644befdce357cd144276
Patrick Brisacier <pbrisacier@mncc.fr>
2025-10-10 18:40:45 +0200

    Ajout message quand le panier est vide

--- static/main.js
+++ static/main.js
@@ -4,17 +4,21 @@ function renderCart(){
   const cartEl = document.getElementById("cart");
   cartEl.innerHTML = "";
   let total = 0;
-  cart.forEach((it, idx) => {
-    const li = document.createElement("li");
-    li.className = "list-group-item d-flex justify-content-between align-items-center";
-    li.innerHTML = `<div>${it.name} x${it.qty} <small>${(it.price*it.qty).toFixed(2)}€</small></div>
-                    <div>
-                      <button class="btn btn-sm btn-outline-secondary me-1" onclick="dec(${idx})">-</button>
-                      <button class="btn btn-sm btn-outline-secondary" onclick="inc(${idx})">+</button>
-                    </div>`;
-    cartEl.appendChild(li);
-    total += it.price * it.qty;
-  });
+  if (cart.length === 0) {
+      cartEl.innerHTML = '<li class="list-group-item text-muted">Panier vide. <br/><small><em>Cliquer sur les boutons des produits pour le remplir.</em></small></li>';
+  } else {
+    cart.forEach((it, idx) => {
+      const li = document.createElement("li");
+      li.className = "list-group-item d-flex justify-content-between align-items-center";
+      li.innerHTML = `<div>${it.name} x${it.qty} <small>${(it.price*it.qty).toFixed(2)}€</small></div>
+                      <div>
+                        <button class="btn btn-sm btn-outline-secondary me-1" onclick="dec(${idx})">-</button>
+                        <button class="btn btn-sm btn-outline-secondary" onclick="inc(${idx})">+</button>
+                      </div>`;
+      cartEl.appendChild(li);
+      total += it.price * it.qty;
+    });
+  }
   document.getElementById("total").innerText = total.toFixed(2);
 }
 
@@ -37,7 +41,7 @@ document.getElementById("clear").addEventListener("click", () => {
 });
 
 document.getElementById("send").addEventListener("click", async () => {
-  if(cart.length === 0){ alert("Panier vide"); return; }
+  if(cart.length === 0){ alert("Panier vide."); return; }
   const payload = { items: cart };
   const resp = await fetch("/order", {
     method: "POST",
